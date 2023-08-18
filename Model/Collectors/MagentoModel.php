49d2060810dc132ea9578b6cec38eceff76f198b
Clive Walkden <clive.walkden@magepulse.com>
2023-08-18 22:57:11 +0100

    Added copyright notice Changed config path Added .gitignore Added translations

--- Model/Collectors/MagentoModel.php
+++ Model/Collectors/MagentoModel.php
@@ -1,4 +1,20 @@
 <?php
+/*
+ * MagePulse
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MagePulse Proprietary EULA
+ * that is bundled with this package in the file LICENSE.
+ * It is also available through the world-wide-web at this URL:
+ * https://magepulse.com/legal/magento-license/
+ *
+ * @category    MagePulse
+ * @package     MagePulse_Collector
+ * @copyright   Copyright (c) MagePulse (https://magepulse.com)
+ * @license     https://magepulse.com/legal/magento-license/  MagePulse Proprietary EULA
+ *
+ */
 
 namespace MagePulse\Collector\Model\Collectors;
 
