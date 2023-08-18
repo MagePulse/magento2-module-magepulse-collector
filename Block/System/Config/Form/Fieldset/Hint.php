2835465d82f85af3030894d8fc3ac1b98f01a802
Clive Walkden <clive@sozodesign.co.uk>
2023-08-18 23:29:06 +0100

    Added Admin Hint block Translations updated Output stopped if the module is disabled

---
+++ Block/System/Config/Form/Fieldset/Hint.php
@@ -0,0 +1,8 @@
+<?php
+
+class Hint extends MagePulse\Core\Block\System\Config\Form\Fieldset\AbstractHint
+{
+    protected $moduleCode = 'MagePulse_Collector';
+
+    protected $moduleName = 'MagePulse Collector';
+}
