56c342e0a76391a8441743a6bc5abcbc9a071593
Clive Walkden <clive@sozodesign.co.uk>
2023-08-18 23:33:30 +0100

    Fixed incorrect Hint class

--- Block/System/Config/Form/Fieldset/Hint.php
+++ Block/System/Config/Form/Fieldset/Hint.php
@@ -1,6 +1,26 @@
 <?php
+/*
+ * MagePulse
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MagePulse Proprietary EULA
+ * that is bundled with this package in the file LICENSE.
+ * It is also available through the world-wide-web at this URL:
+ * https://magepulse.com/legal/magento-license/
+ *
+ * @category    MagePulse
+ * @package     MagePulse_Collector
+ * @copyright   Copyright (c) MagePulse (https://magepulse.com)
+ * @license     https://magepulse.com/legal/magento-license/  MagePulse Proprietary EULA
+ *
+ */
 
-class Hint extends MagePulse\Core\Block\System\Config\Form\Fieldset\AbstractHint
+namespace MagePulse\Collector\Block\System\Config\Form\Fieldset;
+
+use MagePulse\Core\Block\System\Config\Form\Fieldset\AbstractHint;
+
+class Hint extends AbstractHint
 {
     protected $moduleCode = 'MagePulse_Collector';
 
